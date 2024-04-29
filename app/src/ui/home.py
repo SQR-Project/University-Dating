ce74b3f904bda9c262823fb6beac79de96ab1f0f
k.tyulebaeva <k.tyulebaeva@tinkoff.ru>
2024-04-29 19:10:07 +0300

    delete home.py

--- app/src/ui/home.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-
-def app():
-    if not st.session_state["loggedin"]:  # pragma: no mutate
-        st.write("To view your matches please log in")  # pragma: no mutate
-    else:
-        st.write("Here soon to be matches")  # pragma: no mutate
